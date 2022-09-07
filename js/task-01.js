733b90d7781c33efa0b56316140ab87a2b101a35
Olena <kolyada.elena78@gmail.com>
2022-09-07 21:39:51 +0300

    add

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+// const categories = document.querySelector(`#categories`);
+// const itemsRef = document.querySelectorAll(".item");
+// console.log(`Number of categories:${categories.children.length}`);
+
+// itemsRef.forEach((item) => {
+//   const titleCategory = item.querySelector("h2");
+//   const quantityItem = item.querySelectorAll("li");
+//   console.log(`Category: ${titleCategory.textContent}`);
+//   console.log(`Elements: ${quantityItem.length}`);
+// });
