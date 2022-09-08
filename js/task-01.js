0568f3e3c271bda678a9d99c6dc78611e891610f
Olena <kolyada.elena78@gmail.com>
2022-09-08 20:12:25 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,9 @@
-// const categories = document.querySelector(`#categories`);
-// const itemsRef = document.querySelectorAll(".item");
-// console.log(`Number of categories:${categories.children.length}`);
+const itemsRef = document.querySelectorAll(".item");
+console.log(`Number of categories:${items.length}`);
 
-// itemsRef.forEach((item) => {
-//   const titleCategory = item.querySelector("h2");
-//   const quantityItem = item.querySelectorAll("li");
-//   console.log(`Category: ${titleCategory.textContent}`);
-//   console.log(`Elements: ${quantityItem.length}`);
-// });
+itemsRef.forEach((item) => {
+  const titleCategory = item.querySelector("h2");
+  const quantityItem = item.querySelectorAll("li");
+  console.log(`Category: ${titleCategory.textContent}`);
+  console.log(`Elements: ${quantityItem.length}`);
+});
