357606d3b72567dc58710bf00c2b4fa7fd7b8b2c
Olena <kolyada.elena78@gmail.com>
2022-09-11 22:30:10 +0300

    add

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const ref = {
+  changeColorBtn: document.querySelector(".change-color"),
+  color: document.querySelector(".color"),
+  body: document.querySelector("body"),
+};
+ref.changeColorBtn.addEventListener("click", changeColorBtnClick);
+
+function changeColorBtnClick() {
+  const currentColor = getRandomHexColor();
+  ref.color.textContent = currentColor;
+  ref.body.style.backgroundColor = currentColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
